b3d95a3ba13abdc73feb00c1e241c5978a903120
1ta <ektaghule72@gmail.com>
2024-07-23 18:28:56 +0530

    numbers

--- numbers.py
+++ numbers.py
@@ -1,7 +1,31 @@
-#there are 3 numeric type in python int, float, complex.
-x= 19
+#there are 3 numeric type in python int, float, complex. (comple numbers are the numbers with "j" as the imaginary part)
+x=19
 y=57.4
 z=7j
 print("Int:",x)
 print("Float:",y)
 print("Complex:",z)
+
+#you can convert these numbers from one type to other using int(), float() and complex() methods.
+a=7
+b=6.4
+c=9j
+
+print("Before converting:")
+print(a)
+print(b)
+print(c)
+
+#converting
+p=float(a)
+q=int(b)
+r=complex(a)
+
+print("After converting:")
+print(p)
+print(q)
+print(r)
+
+print(type(p))
+print(type(q))
+print(type(r))
