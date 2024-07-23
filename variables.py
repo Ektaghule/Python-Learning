256dc16817ee9e561d806c59c56d5d147ba9a789
1ta <ektaghule72@gmail.com>
2024-07-23 17:42:11 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,34 @@
+#creating variables
+e= 1200
+f="ekta"
+print(e)
+print(f)
+
+# variable can change type after they have been declared.
+a=100
+a="Hey"
+print(a)
+
+#you can declare variable by specifying the datatype, using casting.
+x= int(5500)
+y= str("This is my first python program")
+print(x)
+print(y)
+
+#you can identify the variables datatype using the type() function
+u= 700
+d="Learning python"
+print(type(u))
+print(type(d))
+
+#in python you can declare a string variable using single quotes or double quotes also.
+s='Single quote string variable'
+s1="Double quote string variable"
+print(s)
+print(s1)
+
+#here variable names are case sensitive (q and Q are two different variables).
+q=99
+Q='Lemon'
+print(q)
+print(Q)
