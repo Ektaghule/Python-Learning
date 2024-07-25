775a4861fe61d78a943c077220d5bd2ac46012b0
1ta <ektaghule72@gmail.com>
2024-07-25 16:24:50 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,26 @@
+#lists are used to store multiple values in a single variable
+items=["apple","orange","kiwi","mango"]
+print(items)
+
+#you can acces the list items through index munbers
+print(items[2])
+
+#you can also acces the range of items from the list 
+print(items[0:3])
+print(items[-1])
+
+#you can append(add) new items in the list using append() method
+items.append("pineapple")
+
+#you can get the number of items in the lists using len() function
+print(len(items))
+
+#how to join two lists
+list1=["red","green","yellow"]
+list2=["mysql","c","c++","python"]
+l3=list1+list2
+print(l3)
+
+#you can check if a perticular item is present in the list or not using 'in' operator
+print("kiwi" in items)
+print("watermelon" in items)
