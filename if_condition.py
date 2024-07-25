639c7aa97bfa8a4f8a303ea10808040b1df7e088
1ta <ektaghule72@gmail.com>
2024-07-25 20:59:35 +0530

    if_condition

---
+++ if_condition.py
@@ -0,0 +1,31 @@
+#if condition is written by using "if" keyword
+a=20
+b=10
+if a>b:
+    print("a is grater than b")
+
+#Elif contidion, this condition means if the previous conditions are not true, then try this one
+x=15
+y=15
+if x>y:
+    print("x is greater than y")
+elif x==y:
+    print("x and y are equal")
+
+#else keyword is used if any of the given conditions are not true
+p=12
+q=100
+if p>q :
+    print("p is greater than q")
+elif p==q:
+    print("p and q are equal")
+else :
+    print("q is greater than p")
+
+#if..else condition
+e=87
+s=91
+if e>s:
+    print("e is greater than s")
+else :
+    print("s is greater than e")
