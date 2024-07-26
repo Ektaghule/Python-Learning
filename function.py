315b88d4a6042589e5552354fd7678460d2a4fa0
1ta <ektaghule72@gmail.com>
2024-07-26 18:13:06 +0530

    function

---
+++ function.py
@@ -0,0 +1,16 @@
+#function is a block of code which performs the specified task
+# "def" keyword is used to declare a function in python
+def fun_1():
+    print("This is my first function")
+fun_1()
+
+#arguments in function
+def fun_2(color):
+    print("Colors:",color)
+fun_2("pink")
+fun_2("blue")
+fun_2("red")
+
+#"pass" statement is if a function does not have any definition
+def fun_3():
+    pass
