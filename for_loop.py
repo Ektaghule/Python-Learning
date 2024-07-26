ad9bac62893942b88186595c738d41175493ec12
1ta <ektaghule72@gmail.com>
2024-07-26 15:19:22 +0530

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+#for loop is used for iterating over a sequence of a list, a tuple, a dictionary, a set, or a string.
+fruit=["apple","orange","kiwi"]
+for i in fruit:
+    print(i)
+
+#Loop Through a String
+lang="Python"
+for x in lang:
+    print(x)
+
+#for loop with range() unction
+val=range(6)
+for v in val:
+    print(v)
