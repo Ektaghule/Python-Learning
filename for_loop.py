4f3b6274cf48aefc4ef50ac0099d35c2b4e07833
1ta <ektaghule72@gmail.com>
2024-07-29 18:48:13 +0530

    Exercise

--- for_loop.py
+++ for_loop.py
@@ -12,3 +12,56 @@ for x in lang:
 val=range(6)
 for v in val:
     print(v)
+
+#----Exercise----
+#1.After flipping a coin 10 times you got this result,
+#result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
+#Using for loop figure out how many times you got heads
+result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
+head_count=0
+for flip in result:
+    if flip == "heads":
+        head_count += 1
+print("Number of heads:",head_count)
+
+#2.Print square of all numbers between 1 to 10 except even numbers
+for num in range(1,11):
+    if num % 2!=0:
+        print("Square of numbers except even numbers:",num*num)
+
+#3.Your monthly expense list (from Jan to May) looks like this,
+#expense_list = [2340, 2500, 2100, 3100, 2980]
+#Write a program that asks you to enter an expense amount and program should tell you in which month that 
+# expense occurred. If expense is not found then it should print that as well.
+expense_list = [2340, 2500, 2100, 3100, 2980]
+months=["Jan","Feb","March","April","May"]
+user_input=int(input("Enter an expense amount: "))
+for i in range(len(expense_list)):
+    if expense_list[i]== user_input:
+        print("Expense occured in: ",months[i])
+        break
+else:
+    print("Expense not found")
+
+#4.Lets say you are running a 5 km race. Write a program that,
+# i.Upon completing each 1 km asks you "are you tired?"
+# ii.If you reply "yes" then it should break and print "you didn't finish the race"
+# iii.If you reply "no" then it should continue and ask "are you tired" on every km
+# iv.If you finish all 5 km then it should print congratulations message
+for i in range(1,6):
+    tired=input(f"You ran {i}km! Are you tired? (Y/N): ")
+    if tired == "Y":
+        print("You didn't finish the race...")
+        break
+else:
+    print("Congratulations! You finished the race.")
+
+
+#5.Write a program that prints following shape
+# *
+# **
+# ***
+# ****
+# *****
+for i in range(1,6):
+    print("*"*i)
--- function.py
+++ function.py
@@ -14,3 +14,58 @@ fun_2("red")
 #"pass" statement is if a function does not have any definition
 def fun_3():
     pass
+
+#----Exercise----
+#1.Write a function called calculate_area that takes base and height as an input and returns and area of a 
+# triangle. Equation of an area of a triangle is,
+#area = (1/2)*base*height
+import math
+def calculate_area(base,height):
+    area = (1/2)*base*height
+    return area
+
+base=int(input("Enter base: "))
+height=int(input("Enter height: "))
+area=calculate_area(base,height)
+
+print("Area:",area)
+
+#2.Modify above function to take third parameter shape type. It can be either "triangle" or "rectangle". 
+# Based on shape type it will calculate area. Equation of rectangle's area is,
+#rectangle area=length*width
+#If no shape is supplied then it should take triangle as a default shape
+def calculate_area1(base1,height1,shape="triangle"):
+    if shape == "triangle":
+        area1 = (1/2)*base1*height1
+    elif shape == "rectangle":
+        area1 = base1*height1
+    else:
+        print("Invalid shape")
+        return
+    return area1
+
+base1=int(input("Enter base: "))
+height1=int(input("Enter height: "))
+shape=input("Enter shape (triangle or rectangle, default is triangle): ")
+
+area1=calculate_area1(base1,height1,shape)
+if area1:
+    print("Area of the shape: ",area1)
+
+#3.Write a function called print_pattern that takes integer number as an argument and prints following pattern 
+# if input number is 3,
+# *
+# **
+# ***
+#if input is 4 then it should print
+# *
+# **
+# ***
+# ****
+#Basically number of lines it prints is equal to that number.
+def print_pattern(num):
+    for i in range(1,num+1):
+        print("*"*i)
+
+num=int(input("Enter the number: "))
+print_pattern(num)
